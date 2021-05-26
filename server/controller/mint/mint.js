581b11baa4e5c32ef4202c0036217300f0463a93
prathik <37804760+prmali@users.noreply.github.com>
2021-05-26 00:29:25 -0700

    file implementation + fixes

--- server/controller/mint/mint.js
+++ server/controller/mint/mint.js
@@ -2,40 +2,46 @@ const { pinata, wallet } = require("../../utils");
 const { ethers } = require("ethers");
 
 const fs = require("fs");
+const chalk = require("chalk");
+const path = require("path");
 
-const SINGLEASSET_ABI = require("../abis/SingleAsset.json");
+const SINGLEASSET_ABI = require("../../abis/SingleAsset.json");
 const CONTRACT_ADDRESS = "0x83f6A3231149b38C32ebb2993a2cC095cBb54B51";
+const WALLET_ADDRESS_1 = "0xbA842b7DA417Ba762D75e8F99e11c2980a8F8051";
+
+const test_data_1 = require("../../test_data_1.json");
+const test_image_1 = path.join(__dirname, "../../test_image_1.png");
 
 const uploadImage = async (image) => {
 	try {
 		const readableStream = fs.createReadStream(image);
-		const uri = pinata.pinFileToIPFS(readableStream);
+		const uri = await pinata.pinFileToIPFS(readableStream);
 		if (uri.IpfsHash) return uri.IpfsHash;
-		throw "unable to upload image";
+		//throw "unable to upload image";
 	} catch (err) {
-		console.log(err);
+		console.log(chalk.red(err));
 	}
 };
 
 const uploadMetadata = async (metadata) => {
 	try {
-		const uri = pinata.pinJSONToIPFS(metadata);
+		const uri = await pinata.pinJSONToIPFS(metadata);
 		if (uri.IpfsHash) return uri.IpfsHash;
-		throw "unable to upload image";
+		//throw "unable to upload json";
 	} catch (err) {
-		console.log(err);
+		console.log(chalk.red(err));
 	}
 };
 
 const pushToPinata = async (metadata, image) => {
 	try {
-		const image_uri = await uploadImage(image);
-		metadata["image"] = image_uri;
+		const asset_uri = await uploadImage(image);
+		metadata["image"] = asset_uri;
 		const metadata_uri = await uploadMetadata(metadata);
-		if (metadata_uri) return metadata_uri;
-		throw "unable to push to pinata";
+		if (metadata_uri) return { metadata_uri, asset_uri };
+		//throw "unable to push to pinata";
 	} catch (err) {
-		console.log(err);
+		console.log(chalk.red(err));
 	}
 };
 
@@ -48,21 +54,41 @@ async function getSingleAssetContract() {
 		);
 		return contract;
 	} catch (err) {
-		console.log(err);
+		console.log(chalk.red(err));
 	}
 }
 
-export default async function (target, metadata, image) {
+const mint = async (target, metadata, image) => {
 	try {
 		const contract = await getSingleAssetContract();
-		const uri = pushToPinata(metadata, image);
-		await contract.mint(target, uri);
+		const { metadata_uri, asset_uri } = await pushToPinata(metadata, image);
+		const transaction = await contract.mint(target, metadata_uri);
+		const receipt = await transaction.wait();
+		const tokenId = ethers.utils.arrayify(
+			receipt.events[0].args.tokenId["_hex"]
+		)[0];
 
-		console.log("successfully minted");
+		console.log(
+			"🌿 Successfully minted a new NFT:" +
+				"\nToken ID: " +
+				chalk.magenta(tokenId) +
+				"\nMetadata URI: " +
+				chalk.magenta(metadata_uri) +
+				"\nAsset URI: " +
+				chalk.magenta(asset_uri) +
+				"\nTarget: " +
+				chalk.magenta(target) +
+				"\nFrom: " +
+				chalk.magenta(transaction.from)
+		);
 	} catch (err) {
-		console.log(err);
+		console.log(chalk.red(err));
 	}
-}
+};
+
+//mint(WALLET_ADDRESS_1, test_data_1, test_image_1);
+
+export default mint;
 
 /*
 async function mintAsset(target, uri) {
